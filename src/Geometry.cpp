6f32e4063010cbbaf3926a3a9f8460e1d98c97f7
Owen Kaluza <owen.kaluza@monash.edu>
2015-09-28 17:43:17 +1000

    Fix failure to plot z-axis aligned vectors and draw lines as full vector length

--- src/Geometry.cpp
+++ src/Geometry.cpp
@@ -825,8 +825,11 @@ void Geometry::drawVector(DrawingObject *draw, float pos[3], float vector[3], fl
      rvector.normalise();
      float rangle = RAD2DEG * rvector.angle(Vec3d(0.0, 0.0, 1.0));
      //Axis of rotation = vec x [0,0,1] = -vec[1],vec[0],0
-     Vec3d rvec = Vec3d(-rvector.y, rvector.x, 0);
-     rot.fromAxisAngle(rvec, rangle);
+     if (rangle > 0.0)
+     {
+        Vec3d rvec = Vec3d(-rvector.y, rvector.x, 0);
+        rot.fromAxisAngle(rvec, rangle);
+     }
 
    // Negative scale? Flip vector
    if (scale < 0)
@@ -868,8 +871,8 @@ void Geometry::drawVector(DrawingObject *draw, float pos[3], float vector[3], fl
       Vec3d vertex0 = Vec3d(0,0,-length);
       Vec3d vertex = translate + rot * vertex0;
       read(draw, 1, lucVertexData, vertex.ref());
-      vertex0.z = -headD;
-      vertex = translate + rot * vertex0;
+      vertex0.z = 0;
+      vertex = translate + vertex0;
       read(draw, 1, lucVertexData, vertex.ref());
       return;
    }
