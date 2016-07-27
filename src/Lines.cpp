abf2ea6240544f1e760cf4b318189b84196bba1a
Owen Kaluza <owen.kaluza@monash.edu>
2016-07-27 13:49:43 +1000

    Line width bug fix for vector plots
    Object "scaling" property was being passed to lines sub-renderer and
    applied to line width resulting in vector arrow shafts being too wide

--- src/Lines.cpp
+++ src/Lines.cpp
@@ -170,7 +170,9 @@ void Lines::update()
       geom[i]->draw->properties.data["lit"] = true; //Override lit
       //Draw as 3d cylinder sections
       int quality = 4 * (int)props["glyphs"];
-      float scaling = (float)props["scaling"] * (float)props["scalelines"];
+      float scaling = props["scalelines"];
+      //Don't apply object scaling to internal lines objects
+      if (!internal) scaling *= (float)props["scaling"];
       float radius = scaling*0.1;
       float* oldpos = NULL;
       Colour colour;
@@ -252,7 +254,9 @@ void Lines::draw()
         setState(i, prog);
 
         //Lines specific state
-        float scaling = (float)props["scaling"] * (float)props["scalelines"];
+        float scaling = props["scalelines"];
+        //Don't apply object scaling to internal lines objects
+        if (!internal) scaling *= (float)props["scaling"];
         float lineWidth = (float)props["linewidth"] * scaling * view->scale2d; //Include 2d scale factor
         if (lineWidth <= 0) lineWidth = scaling;
         glLineWidth(lineWidth);
