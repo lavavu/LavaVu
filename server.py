c1ec0037809dd1429a2876d406a3584b5aeee611
Owen Kaluza <owen.kaluza@monash.edu>
2016-10-26 14:28:03 +1100

    Python server update, use jpeg frames at default resolution

--- server.py
+++ server.py
@@ -3,34 +3,33 @@ lavavu = None
 import SimpleHTTPServer
 import SocketServer
 import urllib
+import base64
 
 lavavu = None
 
 class LVRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         global lavavu
+        start = len('data:image/jpeg;base64,')
         if self.path.find('image') > 0:
             self.send_response(200)
-            self.send_header('Content-type', 'image/png')
+            self.send_header('Content-type', 'image/jpeg')
             self.end_headers()
-            img = lavavu.app.image("", lavavu.res[0], lavavu.res[1])
-            img = img[21:] #Strip data:image/png;base64,
-            import base64
+            img = lavavu.frame()
+            img = img[start:]
             self.wfile.write(base64.b64decode(img))
         elif self.path.find('command=') > 0:
             pos1 = self.path.find('=')
             pos2 = self.path.find('?')
             if pos2 < 0: pos2 = len(self.path)
             cmds = urllib.unquote(self.path[pos1+1:pos2])
-            #lavavu.parseCommands(cmd)
             lavavu.commands(cmds.split(';'))
             if self.path.find('icommand=') > 0:
                 self.send_response(200)
-                self.send_header('Content-type', 'image/png')
+                self.send_header('Content-type', 'image/jpeg')
                 self.end_headers()
-                img = lavavu.app.image("", lavavu.res[0], lavavu.res[1])
-                img = img[21:] #Strip data:image/png;base64,
-                import base64
+                img = lavavu.frame()
+                img = img[start:]
                 self.wfile.write(base64.b64decode(img))
             else:
                 self.send_response(200)
