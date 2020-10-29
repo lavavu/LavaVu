293ab1ebe9c94f3042a2416ea4fd96856b6a6938
Owen Kaluza <owen.kaluza@monash.edu>
2020-10-29 21:07:39 +1100

    More fixes

--- conda/build.sh
+++ conda/build.sh
@@ -5,7 +5,7 @@ echo STDLIB_DIR $STDLIB_DIR
 #export LV_LIB_DIRS=${MESA_PATH/lib/x86_64-linux-gnu
 #export LV_INC_DIRS=${MESA_PATH}/include python setup.py install
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-python setup.py install -vv
+$PYTHON setup.py install -vv
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 #WHEEL=$(python setup.py wheelname)
 #echo Using wheel: $WHEEL
