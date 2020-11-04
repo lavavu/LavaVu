4e6e64dc265e10956a10e6a57a13d38762d1432e
Owen Kaluza <owen.kaluza@monash.edu>
2020-11-04 13:51:29 +1100

    Try two conda builds glfw and osmesa

--- conda/build.sh
+++ conda/build.sh
@@ -4,7 +4,7 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 ls $PREFIX/lib
 export LV_LIB_DIRS=$PREFIX/lib
 export LV_INC_DIRS=$PREFIX/include
-export LV_GLFW=1
+#export LV_GLFW=1
 $PYTHON setup.py install -vv
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 #WHEEL=$(python setup.py wheelname)
