eb18756d19719a41bf0e5b6b172577db2a594d9c
Owen Kaluza <owen@kaluza.id.au>
2025-02-11 17:32:36 +1100

    Tracer module - complete rewrite from scratch
    - Use RegularGridInterpolator, kdtree would only be necessary for
    unstructured point interpolation
    - Supports 2d or 3d vector fields
    - Handles visualisation

--- lavavu/tracers.py
+++ lavavu/tracers.py
@@ -1,124 +1,194 @@
 """
-Warning! EXPERIMENTAL:
- these features and functions are under development, will have bugs,
- and may be heavily modified in the future
-
 Tracer particles in a vector field
-Uses a KDTree to find nearest vector to advect the particles
 
-- Requires scipy.spatial
+- Requires scipy.interpolate
 """
 import numpy
 import os
 import sys
 import random
-import math
-from scipy import spatial
+from scipy.interpolate import RegularGridInterpolator
 
-#Necessary? for large trees, detect?
-#sys.setrecursionlimit(10000)
-
-class TracerState(object):
-    def __init__(self, verts, N=5000):
-        self.tree = spatial.cKDTree(verts)
-        self.tracers = None
-        self.steps = [0]*N
-        self.values = None
-        self.positions = None
-        self.velocities = None
-
-def trace_particles(state, verts, vecs, N=5000, limit=0.5, speed=1.0, noise=0.0, height=None):
+def random_particles(count, lowerbound=[0,0,0], upperbound=[1,1,1], dims=3):
     """
-    Take a list of tracer vertices and matching velocity grid points (verts) & vectors (vecs)
-    For each tracer
-
-    - find the nearest velocity grid point
-    - if within max dist: Multiply position by velocity vector
-    - otherwise: Generate a new start position for tracer
-
-    Parameters
-    ----------
-    state : TracerState
-        Object returned from first call, pass None on first pass
-    verts : array or list
-        vertices of the vector field
-    vecs : array or list
-        vector values of the vector field
-    N : int
-        Number of particles to seed
-    limit : float
-        Distance limit over which tracers are not connected,
-        For example if using a periodic boundary, setting limit to
-        half the bounding box size will prevent tracer lines being
-        connected when passing through the boundary
-    speed : float
-        Speed multiplier, scaling factor for the velocity taken from the vector values
-    noise : float
-        A noise factor, if set a random value is generated, multiplied by noise factor
-        and added to each new position
-    height : float
-        A fixed height value, all positions will be given this height as their Z component
-
-    Returns
-    -------
-    TracerState
-        Object to hold the tracer state and track particles
-        Pass this as first paramter on subsequent calls
+    Return an array of *count* 3d vertices of random particle positions
+    Minimum and maximum values defined by lowerbound and upperbound
     """
-
-    #KDstate.tree for finding nearest velocity grid point
-    if state is None:
-        state = TracerState(verts, N)
-
-    lastid = 0
-    def rand_vert():
-        #Get a random velocity grid point
-        lastid = random.randint(0, len(verts)-1)
-        pos = verts[lastid]
-        #Generate some random noise to offset
-        noise3 = numpy.array((0.,0.,0.))
-        if noise > 0.0:
-            noise3 = numpy.random.rand(3) * noise
-        #Fixed height?
-        if height:
-            noise3[2] = height
-        #Return the sum
-        return pos + noise3
-
-    if state.positions is None:
-        state.positions = numpy.zeros(shape=(N,3))
-        state.velocities = numpy.zeros(shape=(N,3))
-        state.values = numpy.zeros(shape=(N))
-        for i in range(N):
-            state.positions[i] = rand_vert()
-            state.steps[i] = 0
-            state.values[i] = numpy.linalg.norm(vecs[lastid])
-
-    #Query all tracer state.positions
-    q = state.tree.query(state.positions, k=1)
-    for r in range(len(q[0])):
-        #print("result, distance, point")
-        #print(r, q[0][r], state.tree.data[q[1][r]], state.positions[r])
-
-        #Increasing random chance as steps exceed 5 of a new start pos
-        if random.randint(0,state.steps[r]) > 5:
-            #Pick a new random grid vertex to start from
-            #(Must be farther away than distance criteria)
-            old = numpy.array(state.positions[r])
-            while True:
-                state.positions[r] = rand_vert() 
-                if numpy.linalg.norm(state.positions[r]-old) > limit:
-                    break
-            state.steps[r] = 0
-            state.values[r] = numpy.linalg.norm(vecs[lastid])
+    p = [None] * dims
+    for c in range(dims):
+        if lowerbound[c] == upperbound[c]:
+            p[c] = numpy.zeros(shape=(count)) + lowerbound[c]
         else:
-            #Index of nearest grid point is in q[1][r]
-            #Lookup vector at this index, multiply by position to get delta and add
-            state.velocities[r] = vecs[q[1][r]] #Store velocity
-            state.positions[r] += speed * vecs[q[1][r]]
-            #Increment step tracking
-            state.steps[r] += 1
-            state.values[r] = numpy.linalg.norm(vecs[q[1][r]])
+            p[c] = numpy.random.uniform(low=lowerbound[c], high=upperbound[c], size=count)
 
-    return state
+    return numpy.stack(p).T
+
+class Tracers():
+    def __init__(self, grid, count=1000, lowerbound=None, upperbound=None, limit=None, age=4, respawn_chance=0.2, speed_multiply=1.0, height=0.0, viewer=None):
+        """
+        Seed random particles into a vector field and trace their positions
+
+        Parameters
+        ----------
+        grid : list of coord arrays for each dimension as expected by RegularGridInterpolator,
+            or a numpy array of 2d or 3d vertices, which will be converted before being sent to the interpolator
+            Object returned from first call, pass None on first pass
+        count : int
+            Number of particles to seed and track
+        lowerbound : optional minimum vertex point defining particle bounding box,
+            if not provided will be taken from grid lower corner
+        upperbound : optional maximum vertex point defining particle bounding box,
+            if not provided will be taken from grid upper corner
+        limit : float
+            Distance limit over which tracers are not connected,
+            For example if using a periodic boundary, setting limit to
+            half the bounding box size will prevent tracer lines being
+            connected when passing through the boundary
+        age : int
+            Minimum particle age in steps after which particle can be deleted and respawned, defaults to 4
+        respawn : float
+            Probability of respawning, after age reached, default 0.2 ==> 1 in 5 chance of deletion
+        speed_multiply : float
+            Speed multiplier, scaling factor for the velocity taken from the vector values
+        height : float
+            A fixed height value, all positions will be given this height as their Z component
+        viewer : lavavu.Viewer
+            Viewer object for plotting functions
+        """
+        if len(grid) == 2:
+            self.gridx = grid[0]
+            self.gridy = grid[1]
+            self.gridz = numpy.array((height, height))
+            self.dims = 2
+        elif len(grid) == 3:
+            self.gridx = grid[0]
+            self.gridy = grid[1]
+            self.gridz = grid[2]
+            self.dims = 3
+        elif isinstance(grid, numpy.ndarray) and grid.shape[1] == 3:
+            self.gridx = grid[::,0]
+            self.gridy = grid[::,1]
+            self.gridz = grid[::,2]
+            self.dims = 3
+        elif isinstance(grid, numpy.ndarray) and grid.shape[1] == 2:
+            self.gridx = grid[::,0]
+            self.gridy = grid[::,1]
+            self.gridz = numpy.array((height, height))
+            self.dims = 2
+        else:
+            raise(ValueError('Grid needs to be array of 2d/3d vertices, or arrays of vertex coords (x, y, [z])'))
+
+        self.count = count
+        if lowerbound is None:
+            lowerbound = (self.gridx[0], self.gridy[0], self.gridz[0])
+        if upperbound is None:
+            upperbound = (self.gridx[-1], self.gridy[-1], self.gridz[-1])
+        self.positions = random_particles(self.count, lowerbound, upperbound, self.dims)
+        self.old_pos = numpy.zeros_like(self.positions)
+        self.lowerbound = lowerbound
+        self.upperbound = upperbound
+        self.velocities = None
+        self.steps = [0]*count
+        self.speed = numpy.zeros(shape=(count))
+        self.ages = numpy.zeros(shape=(count))
+        self.interp = None
+        if limit is None:
+            limit = 0.1 * (abs(self.gridx[-1] - self.gridx[0]) + abs(self.gridy[-1] - self.gridy[0]))
+        self.limit = limit
+        self.age = age
+        self.respawn_chance = respawn_chance
+        self.speed_multiply = speed_multiply
+        self.height = height
+
+        self.lv = viewer
+        self.points = None
+        self.arrows = None
+        self.tracers = None
+
+
+    def respawn(self, r):
+        #Dead or out of bounds particle, start at new position
+        #Loop until new position further from current position than limit
+        old_pos = self.positions[r]
+        pos = numpy.array([0.] * self.dims)
+        for i in range(10):
+            pos = random_particles(1, self.lowerbound, self.upperbound, self.dims)
+            dist = numpy.linalg.norm(old_pos - pos)
+            if dist > self.limit*1.01:
+                break
+
+        self.ages[r] = 0
+        self.positions[r] = pos
+
+    def update(self, vectors=None):
+        #Interpolate velocity at all positions,
+        #If vectors not passed, will use previous values
+        if vectors is not None:
+            if self.dims == 2:
+                self.interp = RegularGridInterpolator((self.gridx, self.gridy), vectors, bounds_error=False, fill_value=0.0)
+            else:
+                self.interp = RegularGridInterpolator((self.gridx, self.gridy, self.gridz), vectors, bounds_error=False, fill_value=0.0)
+
+        if self.interp is None:
+            raise(ValueError("No velocity grid, must pass vectors for first call of update()"))
+
+        self.velocities = self.interp(self.positions)
+        self.old_pos = numpy.copy(self.positions)
+
+        for r in range(len(self.velocities)):
+            #Lookup velocity at this index, multiply by position to get delta and add
+            self.speed[r] = numpy.linalg.norm(self.velocities[r])
+            if numpy.isnan(self.speed[r]): self.speed[r] = 0.0
+            if self.speed[r] == 0.0: #numpy.any(numpy.isinf(self.old_pos[r])) or numpy.any(numpy.isinf(self.positions[r])):
+                self.respawn(r)
+            else:
+                self.positions[r] = self.positions[r] + self.speed_multiply * self.velocities[r]
+                self.ages[r] += 1
+
+            #Bounds checks
+            #Chance of killing particle when over age, default 1 in 5 (0.2)
+            if (any(self.positions[r] < self.lowerbound[0:self.dims]) or any(self.positions[r] > self.upperbound[0:self.dims])
+            or (self.ages[r] > self.age and numpy.random.uniform() <= self.respawn_chance)):
+                #if r < 20: print("Kill", r, self.speed[r], numpy.isnan(self.speed[r])) # [0] == numpy.nan)
+                #self.positions[r] = numpy.array([numpy.inf] * self.dims)
+                #self.positions[r] = numpy.array([numpy.nan] * self.dims)
+                self.respawn(r)
+                self.velocities[r] = numpy.array([0.0] * self.dims)
+                self.speed[r] = 0.0
+
+        if self.lv:
+            positions = self.positions
+            if self.dims == 2 and self.height != 0:
+                #Convert to 3d and set z coord to height
+                shape = list(positions.shape)
+                shape[-1] = 3
+                positions = numpy.zeros(shape)
+                positions[::,0:2] = self.positions
+                positions[::,2] = numpy.array([self.height] * shape[0])
+            if self.points:
+                self.points.vertices(positions)
+                if len(self.points["colourmap"]):
+                    self.points.values(self.speed)
+            if self.arrows:
+                self.arrows.vectors(self.velocities)
+                self.arrows.vertices(positions)
+                if len(self.arrows["colourmap"]):
+                    self.arrows.values(self.speed)
+
+            if self.tracers:
+                self.tracers.vertices(positions)
+                if len(self.tracers["colourmap"]):
+                    self.tracers.values(self.speed)
+
+    def plot_points(self, **kwargs):
+        if self.lv is not None and self.points is None:
+            self.points = self.lv.points('tracer_points', **kwargs)
+
+    def plot_arrows(self, **kwargs):
+        if self.lv is not None and self.arrows is None:
+            self.arrows = self.lv.vectors('tracer_arrows', **kwargs)
+
+    def plot_tracers(self, **kwargs):
+        if self.lv is not None and self.tracers is None:
+            self.tracers = self.lv.tracers('tracers', dims=self.count, limit=self.limit, **kwargs)
 
